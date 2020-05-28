8c0c7b3472e29d4f97af30491dc5de37af7ce820
GangjunLee <admin@gangjun.dev>
2020-05-28 22:36:33 +0900

    'fixed'

--- src/components/SendMail.vue
+++ src/components/SendMail.vue
@@ -3,36 +3,21 @@
     <v-container>
       <v-row>
         <v-col cols="6" md="3">
-          <v-text-field
-            v-model="form.from"
-            :rules="form.fromRule"
-            label="보내는사람"
-            required
-          />
+          <v-text-field v-model="form.from" :rules="form.fromRule" label="보내는사람" required />
         </v-col>
 
         <v-col cols="6" md="3">
-          <v-text-field
-            v-model="form.to"
-            :rules="form.toRule"
-            label="받는사람"
-            required
-          />
+          <v-text-field v-model="form.to" :rules="form.toRule" label="받는사람" required />
         </v-col>
       </v-row>
-      <v-text-field
-        v-model="form.subject"
-        :rules="form.subjectRule"
-        label="제목"
-        required
-      />
+      <v-text-field v-model="form.subject" :rules="form.subjectRule" label="제목" required />
       <div v-if="!template">
         <v-textarea label="본문" v-model="form.text" />
       </div>
       <div v-else>
         <v-label>초대 템플릿</v-label>
-        <v-row
-          ><v-col cols="6" md="3">
+        <v-row>
+          <v-col cols="6" md="3">
             <v-text-field
               v-model="invitation.title"
               :rules="invitation.titleRule"
@@ -46,32 +31,18 @@
               :rules="invitation.discordRule"
               label="디스코드"
               required
-          /></v-col>
+            />
+          </v-col>
         </v-row>
-        <v-textarea
-          label="본문"
-          v-model="invitation.content"
-          :rules="invitation.contentRule"
-        />
+        <v-textarea label="본문" v-model="invitation.content" :rules="invitation.contentRule" />
       </div>
       <div class="d-flex">
-        <v-btn color="success" class="mr-4" @click="sendMail">
-          SendMail
-        </v-btn>
-        <v-btn color="error" class="mr-4" @click="resetForm">
-          Form Reset
-        </v-btn>
+        <v-btn color="success" class="mr-4" @click="sendMail">SendMail</v-btn>
         <v-btn color="warning" class="mr-4" @click="template = !template">
-          {{ template ? 'send Only Text' : ' Send With Template' }}
+          {{
+          template ? 'send Only Text' : ' Send With Template'
+          }}
         </v-btn>
-        <v-text-field
-          v-if="template"
-          solo
-          v-model="invitation.year"
-          :rules="invitation.yearRule"
-          label="년도"
-          required
-        />
       </div>
     </v-container>
   </v-form>
@@ -88,9 +59,9 @@ export default {
     form: {
       from: 'Team IF <no-reply@teamif.io>',
       fromRule: [v => !!v || '보내는사람은 무조건 적어야합니다'],
-      to: 'me@gangjun.dev',
+      to: '',
       toRule: [v => !!v || '받는사람은 무조건 적어야합니다'],
-      subject: 'a',
+      subject: '',
       subjectRule: [v => !!v || '제목은 무조건 작성하여야 합니다'],
       text: ''
     },
@@ -100,31 +71,20 @@ export default {
       content: '',
       contentRule: [v => !!v || '내용은 무조건 적어야합니다'],
       discord: '',
-      discordRule: [v => !!v || '디스코드 태그는 무조건 적어야합니다'],
-      year: '',
-      yearRule: [v => !!v || '년도는 무조건 적어야합니다']
+      discordRule: [v => !!v || '디스코드 태그는 무조건 적어야합니다']
     }
   }),
-  mounted() {
-    this.invitation.year = new Date().getFullYear().toString()
-  },
   methods: {
     sendMail() {
       this.$refs.form.validate()
       if (this.template) {
-        if (this.invitation.content.includes('\n')) {
-          this.invitation.content = this.invitation.content.split('\n')
-        } else {
-          this.invitation.content = [this.invitation.content]
-        }
         Axios.post(
-          'http://localhost:3000/mail/invitation',
+          'https://shol.xyz:8446/mail/invitation',
           {
             from: this.form.from,
             to: this.form.to,
             subject: this.form.subject,
             title: this.invitation.title,
-            year: this.invitation.year,
             discord: this.invitation.discord,
             content: this.invitation.content
           },
@@ -134,15 +94,20 @@ export default {
               password: config.password
             }
           }
-        ).then(result => {
-          console.log(result.data)
-          alert(
-            '메일이 전송되었습니다.\nctrl+shift+i를 눌러 콘솔에서 로그를 확인하세요'
-          )
-        })
+        )
+          .then(result => {
+            console.log(result.data)
+            alert(
+              '메일이 전송되었습니다.\nctrl+shift+i를 눌러 콘솔에서 로그를 확인하세요'
+            )
+          })
+          .catch(err => {
+            console.log(err)
+            alert('에러가 발생하였습니다. 콘솔에서 확인해주세요')
+          })
       } else {
         Axios.post(
-          'http://localhost:3000/mail',
+          'http://localhost:8446/mail',
           {
             from: this.form.from,
             to: this.form.to,
@@ -155,17 +120,18 @@ export default {
               password: config.password
             }
           }
-        ).then(result => {
-          console.log(result.data)
-          alert(
-            '메일이 전송되었습니다.\nctrl+shift+i를 눌러 콘솔에서 로그를 확인하세요'
-          )
-        })
+        )
+          .then(result => {
+            console.log(result.data)
+            alert(
+              '메일이 전송되었습니다.\nctrl+shift+i를 눌러 콘솔에서 로그를 확인하세요'
+            )
+          })
+          .catch(err => {
+            console.log(err)
+            alert('에러가 발생하였습니다. 콘솔에서 확인해주세요')
+          })
       }
-      this.resetForm()
-    },
-    resetForm() {
-      this.$refs.form.reset()
     }
   }
 }
